b7527813029b0c00e57c18808fcc0182cf911499
paul chenon <chenon.p@gmail.com>
2016-02-16 10:38:25 +0100

    Added facebook connect but without profil picture

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
   resources :users, only: [:show] do
     resources :bikes
     end
