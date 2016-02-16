c5c532ea19c9b13498503295c88861040f60faad
Antoine Loron <loron.antoine@gmail.com>
2016-02-16 18:16:56 +0100

    fixed card height and limited description length to 50char for bikes

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -5,7 +5,7 @@ class Bike < ActiveRecord::Base
   has_attachments :photos, maximum: 1
 
   validates :address, presence: true
-  validates :description, presence: true
+  validates :description, presence: true, length: { maximum: 50 }
   validates :city, presence: true
   validates :kind, inclusion: { in: %w(VTT ville tandem),
     message: "%{value} is not a valid type" }
