1c050029ab51ae68823bc0e39e9e31e4620a48ec
paul chenon <chenon.p@gmail.com>
2016-02-16 19:15:03 +0100

    booking controller review

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,45 @@
+class BookingsController < ApplicationController
+  # before_action :set_user, only: [:new, :create]
+  # before_action :set_bike, only: [:new, :create]
+
+
+  def new
+    @booking = Booking.new
+    @bike = Bike.find(params[:bike_id])
+  end
+
+  def create
+    @booking = @user.booking.new(booking_params)
+    if @booking.save
+      redirect_to bike_path(@bike)
+    else
+      render :new
+    end
+  end
+
+  # def edit
+  # end
+
+  # def update
+  # end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to :back
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :bike_id)
+  end
+
+  # def set_user
+  #   @user = User.find(params[:user_id])
+  # end
+
+  # def set_bike
+  #   @bike = Bike.find(params[:id])
+  # end
+end
