ed1996c420b77a83564136639a7ed608bf8cdba8
Antoine Loron <loron.antoine@gmail.com>
2016-02-17 16:24:28 +0100

    seeded 100 random US addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Bike.destroy_all
-Bike.create(description: "VTT de compet", address: "Rue de la convention", helmet: true, zip: "75015", city: "Paris", kind: "VTT", user_id: 1)
-Bike.create(description: "Velo hollandais", address: "Rue paradis ", helmet: false, zip: "4000", city: "Nantes", kind: "ville", user_id: 1)
+
+100.times do
+Bike.create(description: Faker::Lorem.sentence,
+            address: Faker::Address.street_name,
+            city: Faker::Address.city,
+            kind: ["VTT", "tandem", "ville"].sample,
+            user_id: [1].sample)
+end
+
+
+
+
+
+
+# Bike.create(description: "VTT de compet"
+#             address: "Rue de la convention",
+#             helmet: true, zip: "75015",
+#             city: "Paris",
+#             kind: "VTT",
+#             user_id: 1)
+# Bike.create(description: "Velo hollandais", address: "Rue paradis ", helmet: false, zip: "4000", city: "Nantes", kind: "ville", user_id: 1)
